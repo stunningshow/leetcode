3af172b9c8e035384961fc0f4dfd8f25836bbb51
yujing <stunningshow@gmail.com>
2017-12-21 11:27:02 -0800

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,79 @@
+/******************************************************************************
+ *  Compilation:  javac BinarySearch.java
+ *  Execution:    java BinarySearch whitelist.txt < input.txt
+ *                                  std input    (< read from a file)
+ *  Dependencies: In.java StdIn.java StdOut.java
+ *  Data files:   https://algs4.cs.princeton.edu/11model/tinyW.txt
+ *                https://algs4.cs.princeton.edu/11model/tinyT.txt
+ *                https://algs4.cs.princeton.edu/11model/largeW.txt
+ *                https://algs4.cs.princeton.edu/11model/largeT.txt
+ *
+ *  % java BinarySearch tinyW.txt < tinyT.txt
+ *  50
+ *  99
+ *  13
+ *
+ *  % java BinarySearch largeW.txt < largeT.txt | more
+ *  499569
+ *  984875
+ *  295754
+ *  207807
+ *  140925
+ *  161828
+ *  [367,966 total values]
+ *  
+ ******************************************************************************/
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    /**
+     * This class should not be instantiated.
+     */
+    private BinarySearch() { }
+
+    /**
+     * Returns the index of the specified key in the specified array.
+     *
+     * @param  a the array of integers, must be sorted in ascending order
+     * @param  key the search key
+     * @return index of key in array {@code a} if present; {@code -1} otherwise
+     */
+    public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    @Deprecated
+    public static int rank(int key, int[] a) {
+        return indexOf(a, key);
+    }
+
+    public static void main(String[] args) {
+
+        // read the integers from a file
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
+        
+        // sort the array
+        Arrays.sort(whitelist);
+
+        // read integer key from standard input; print if not in whitelist
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt(); // read every int from the standard input
+           
+            if (BinarySearch.indexOf(whitelist, key) == -1)
+               StdOut.println(key);
+            //else StdOut.println(BinarySearch.indexOf(whitelist, key)); print the rank if you want~
+        }
+    }
+}
